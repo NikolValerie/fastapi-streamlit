d586f48c61f528f0da4173234c4eac13547860a3
Pavel <duminpn@gmail.com>
2024-02-14 15:08:20 +0300

    ports changed

--- front/main.py
+++ front/main.py
@@ -11,14 +11,14 @@ def main():
     if st.button("Classify!") and image is not None:
         st.image(image)
         files = {"file": image.getvalue()}
-        res = requests.post("http://127.0.0.1:8002/classify", files=files)
+        res = requests.post("http://127.0.0.1:8000/classify", files=files)
         st.write(json.loads(res.text)['prediction'])
 
     txt = st.text_input('here')
 
     if st.button('send'):
         dat = {'text' : txt}
-        res = requests.post("http://127.0.0.1:8003/clf_text", json=dat)
+        res = requests.post("http://127.0.0.1:8000/clf_text", json=dat)
 
 if __name__ == '__main__':
     main()
